8330fb1da2caa44f958f4cf965f6fd06736ae660
Jeffrey van Rossum <jeffrey@vanrossum.dev>
2025-02-12 16:59:22 +0100

    wip

--- src/Console/NewCommand.php
+++ src/Console/NewCommand.php
@@ -65,7 +65,7 @@ class NewCommand extends Command
         }
 
         $commands[] = "cd \"$workingDirectory\"";
-        $commands[] = "git clone -b master https://github.com/jeffreyvr/tailpress.git . --q";
+        $commands[] = "git clone -b 4.x https://github.com/jeffreyvr/tailpress.git . --q";
 
         if (($process = $this->runCommands($commands, $input, $output))->isSuccessful()) {
             if ($name = $input->getOption('name')) {
